88bf0e065fb80d1de20d65647d5465a48fc967da
Danilo Lima <danilo.lima@leato.io>
2023-06-22 09:19:10 -0300

    feat: adds the index page and button assets

---
+++ dist/script.js
@@ -0,0 +1,20 @@
+$("input").intlTelInput({
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js",
+  initialCountry: "auto",
+  geoIpLookup: (callback) => {
+    fetch("https://ipapi.co/json")
+      .then((res) => res.json())
+      .then((data) => callback(data.country_code))
+      .catch(() => callback("br"))
+  },
+});
+
+$("#chatButton").on('click', (event) => {
+  event.preventDefault();
+
+  const countryCode = $("input").intlTelInput("getSelectedCountryData").dialCode;
+  const phoneNumber = $("input").val();
+  const fullPhone = `${countryCode}${phoneNumber}`;
+
+  window.open(`https://wa.me/${fullPhone}`, '_blank');
+});
